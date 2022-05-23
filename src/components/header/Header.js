ad1584d0886590600cf689ae7699d83424029472
Koko-Dev <koko.webdev@gmail.com>
2022-05-23 14:12:11 -0400

    Header.js: Surround icon divs with .headerContainer > .headerList; in header.css, style horizontally with appropriate spacing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,25 +7,29 @@ const Header = () => {
 	return (
 		<div className="header">
 			<div className="headerList">
-				<div className="headerListItem">
-					<FontAwesomeIcon icon={faBed} />
-					<span>Stays</span>
-				</div>
-				<div className="headerListItem">
-					<FontAwesomeIcon icon={faPlane} />
-					<span>Flights</span>
-				</div>
-				<div className="headerListItem">
-					<FontAwesomeIcon icon={faCar} />
-					<span>Car rentals</span>
-				</div>
-				<div className="headerListItem">
-					<FontAwesomeIcon icon={faMagnet} />
-					<span>Attractions</span>
-				</div>
-				<div className="headerListItem">
-					<FontAwesomeIcon icon={faTaxi} />
-					<span>Airport taxis</span>
+				<div className="headerContainer">
+					<div className="headerList">
+						<div className="headerListItem">
+							<FontAwesomeIcon icon={faBed}/>
+							<span>Stays</span>
+						</div>
+						<div className="headerListItem">
+							<FontAwesomeIcon icon={faPlane}/>
+							<span>Flights</span>
+						</div>
+						<div className="headerListItem">
+							<FontAwesomeIcon icon={faCar}/>
+							<span>Car rentals</span>
+						</div>
+						<div className="headerListItem">
+							<FontAwesomeIcon icon={faMagnet}/>
+							<span>Attractions</span>
+						</div>
+						<div className="headerListItem">
+							<FontAwesomeIcon icon={faTaxi}/>
+							<span>Airport taxis</span>
+						</div>
+					</div>
 				</div>
 
 			</div>
