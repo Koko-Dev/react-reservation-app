13c260a3a9e05c82231abf54aea8a6324020a7b2
Koko-Dev <koko.webdev@gmail.com>
2022-05-23 14:29:55 -0400

    Header.js: Move content of last commit one div in

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -30,15 +30,16 @@ const Header = () => {
 							<span>Airport taxis</span>
 						</div>
 					</div>
+					<h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+					{/* /.headerTitle */}
+					<p className="headerDesc">
+						Get rewarded for your travels - unlock instant savings of 10% or more with a free Koko Bookings account
+					</p>
+					<button className="headerBtn">Sign in / Register</button>
+					{/* /.headerBtn */}
+					{/* /.headerDesc */}
 				</div>
-				<h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
-				{/* /.headerTitle */}
-				<p className="headerDesc">
-					Get rewarded for your travels - unlock instant savings of 10% or more with a free Koko Bookings account
-				</p>
-				<button className="headerBtn">Sign in / Register</button>
-				{/* /.headerBtn */}
-				{/* /.headerDesc */}
+
 
 			</div>
 			{/* /.headerList */}
