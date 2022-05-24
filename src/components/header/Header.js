e70b41d65e144d6d6d60de380723b95beb8c7702
Koko-Dev <koko.webdev@gmail.com>
2022-05-24 13:38:37 -0400

    Header.js: Create two more pieces of state - openOptions/setOpenOptions initialized to false and options/setOptions initialized to adult: 1, children: 0, room: 1;  in span.headerSearchText for faPerson, set a listener which calls setOpenOptions(!openOptions); in span body, display options key/value pairs to the screen; initially this is hardcoded with our initialState options
    // Header.js relevant code
    const [openOptions, setOpenOptions] = useState(false);
        const [options, setOptions] = useState({
            adult: 1,
            children: 0,
            room: 1
        });
    
    <div className="headerSearchItem">
        <FontAwesomeIcon icon={faPerson}
                         className="headerIcon"/>
        <span
            className="headerSearchText"
            onClick={() => setOpenOptions(!openOptions)}>
                {`
                    ${options.adult} adult ·
                    ${options.children} children ·
                    ${options.room} room`
                }
        </span>
    </div>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -25,6 +25,14 @@ const Header = () => {
 			key: "selection",
 		},
 	]);
+
+	const [openOptions, setOpenOptions] = useState(false);
+	const [options, setOptions] = useState({
+		adult: 1,
+		children: 0,
+		room: 1
+	});
+
 	return (
 		<div className="header">
 			<div className="headerContainer">
@@ -88,7 +96,15 @@ const Header = () => {
 					<div className="headerSearchItem">
 						<FontAwesomeIcon icon={faPerson}
 						                 className="headerIcon"/>
-						<span className="headerSearchText">2 adults 2 children 1 room</span>
+						<span
+							className="headerSearchText"
+							onClick={() => setOpenOptions(!openOptions)}>
+								{`
+									${options.adult} adult · 
+									${options.children} children · 
+									${options.room} room`
+								}
+						</span>
 					</div>
 					<div className="headerSearchItem">
 						<button className="headerBtn">Search</button>
