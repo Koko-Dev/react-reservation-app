e64cd235d6320a07825af3e62b14e7fdc16d6c48
Koko-Dev <koko.webdev@gmail.com>
2022-05-25 13:32:54 -0400

    PropertyList.js: Add a hotel list with images and text displaying what type of hotel and the number of hotel listings available in each category; Home.js: Import PropteyList and display in return div jsx below div.homeContainer

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import './home.css';
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
 	return (
@@ -12,6 +13,7 @@ const Home = () => {
 				<Featured />
 				<h1 className="homeTitle">Browse by property type</h1>
 			</div>
+			<PropertyList />
 		</div>
 	)
 }
