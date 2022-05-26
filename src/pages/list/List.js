97b6ece205cb17240ce158d70ce0f28a87261691
Koko-Dev <koko.webdev@gmail.com>
2022-05-26 11:13:33 -0400

    List.js: Begin to add Search section destination and check-in-date; create list.css and begin to style .listContainer and .listWrapper

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import  './list.css';
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 
@@ -7,6 +7,29 @@ const List = () => {
 		<div>
 			<Navbar />
 			<Header type="list" />
+			<div className="listContainer">
+				<div className="listWrapper">
+					<div className="listSearch">
+						<h1 className="lsTitle">Search</h1>
+						{/* /.lsTitle */}
+						<div className="lsItem">
+							<label htmlFor="">Destination</label>
+							<input type="text"/>
+						</div>
+						{/* /.lsItem */}
+						<div className="lsItem">
+							<label htmlFor="">Check-in Date</label>
+						</div>
+						{/* /.lsItem */}
+					</div>
+					{/* /.listSearch */}
+					<div className="listResult"></div>
+					{/* /.listResult */}
+				</div>
+				{/* /.listWrapper */}
+			</div>
+			{/* /.listContainer */}
+			
 		</div>
 	)
 }
