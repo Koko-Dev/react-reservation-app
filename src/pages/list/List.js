bb2a481e69ce917c505e30bb84a1483ae6e2b4c9
Koko-Dev <koko.webdev@gmail.com>
2022-05-27 13:55:42 -0400

    List.js:  Import useState from react, add state for destination, date and options; also add state for openDate and set it's initial useState to false

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -2,9 +2,15 @@ import  './list.css';
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const List = () => {
 	const location = useLocation();
+	const [destination, setDestination] = useState(location.state.destination);
+	const [date, setDate] = useState(location.state.date);
+	const [openDate, closeDate] = useState(false);
+	const [options, setOptions] = useState(location.state.options);
+	
 	return (
 		<div>
 			<Navbar />
