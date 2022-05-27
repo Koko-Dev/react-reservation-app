1c301bf9785645f72c29a8c3e32b7b7f85c55303
Koko-Dev <koko.webdev@gmail.com>
2022-05-27 14:21:14 -0400

    List.js: Import format from date-fns and DateRange from react-date-range; fix state for openDate, the second var should be setOpenDate, not coseDate; also, add placeholder {destination{ for <label>Destination</>; and for Check-in-Date span, add onClick prop which sets setOpenDate based on whether openDate is true or false; display the start and end date based on user selection

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -3,12 +3,14 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
 	const location = useLocation();
 	const [destination, setDestination] = useState(location.state.destination);
 	const [date, setDate] = useState(location.state.date);
-	const [openDate, closeDate] = useState(false);
+	const [openDate, setOpenDate] = useState(false);
 	const [options, setOptions] = useState(location.state.options);
 	
 	console.log(location);
@@ -23,14 +25,16 @@ const List = () => {
 						<h1 className="lsTitle">Search</h1>
 						{/* /.lsTitle */}
 						<div className="lsItem">
-							<label htmlFor="">Destination</label>
+							<label htmlFor="" placeholder={destination}>Destination</label>
 							<input type="text"/>
 						</div>
 						{/* /.lsItem */}
 						<div className="lsItem">
 							<label htmlFor="">Check-in Date</label>
-							
-							
+							<span onClick={() => setOpenDate(!openDate)}>{`${format(
+								date[0].startDate,
+								"MM/dd/yyyy"
+							)} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
 						</div>
 						{/* /.lsItem */}
 					</div>
