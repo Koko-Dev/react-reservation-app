064140b60e8048e0fc79001fac420f5ba163d5b1
Koko-Dev <koko.webdev@gmail.com>
2022-05-31 12:15:14 -0400

    Hotel.js: import useState from react, create a piece of state - open/setOpen and set initialState to false; add div.hotelContainer before Header component

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -1,12 +1,19 @@
 import './hotel.css';
+import {useState} from 'react';
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 
 const Hotel = () => {
+	const [open, setOpen] = useState('false');
+	
 	return (
 		<div>
 			<Navbar />
 			<Header type="list"/>
+			<div className="hotelContainer">
+			
+			</div>
+			{/* /.hotelContainer */}
 		</div>
 	)
 }
