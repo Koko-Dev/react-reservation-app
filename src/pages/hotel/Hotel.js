fcdf0e11a62a2d8a5276e263b523d4f154ad325a
Koko-Dev <koko.webdev@gmail.com>
2022-05-31 11:51:28 -0400

    Hotel.js: Import Navbar and Header components and add to return div; App.js: Refactor

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -1,8 +1,13 @@
 import './hotel.css';
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 
 const Hotel = () => {
 	return (
-		<div>Hotel</div>
+		<div>
+			<Navbar />
+			<Header type="list"/>
+		</div>
 	)
 }
 
